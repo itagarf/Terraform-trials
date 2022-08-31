c44baff80f58c7bf32d11108eea4642c187e5073
itagarf <itagarfavour@gmail.com>
2022-08-31 11:14:28 +0100

    debug logging added

---
+++ logs.tf
@@ -0,0 +1,11 @@
+# Enable log tracing to debug especially when there are errors
+# export TF_LOG=TRACE
+
+# terraform plan
+
+# To store in a file:
+# export TF_LOG_PATH=/location/terraform.log
+# head -10 /location/terraform.log
+
+# To disable logging
+# unset TF_LOG_PATH
